3ff5ad9a2954ebad9dd68a348ea67d4b1eefab4e
KSP-Hub <ksp-hub@users.noreply.github.com>
2026-05-17 18:46:24 +0300

    feat(keymap/vial): replace manual Vial layout with stable 4-layer keymap.c matching KLE design

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/keymap.c
@@ -13,6 +13,7 @@ enum charybdis_layers {
 #define PT_Z LT(LAYER_POINTER, KC_Z)
 #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
 
+// Кастомные коды для макросов (привязка в process_record_user)
 enum custom_keycodes {
     MC_LCHG = QK_USER,
     MC_NBSP,
@@ -38,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
          KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LSFT,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-         KC_LCTL, PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  PT_SLSH, KC_RCTL,
+         PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_INS,     KC_N,    KC_M,    KC_COMM, KC_DOT,  PT_SLSH, KC_RALT,
     // ╰───────────────────────╮                              ╰─╯                              ╭───────────────────────╯
                       KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_ENT,  KC_LALT,    KC_BSPC, KC_DEL
     // ╰───────────────────────╯                              ╰─╯                              ╰───────────────────────╯
