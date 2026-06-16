7f7cbd4358d0586de0be98bc649ddf61181adb1e
KSP-Hub <karamin.stanislav@gmail.com>
2026-06-16 18:36:05 +0300

    fix(rules.mk): remove invisible characters causing missing separator error

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
@@ -1,6 +1,3 @@
-cd ~/vial-qmk/keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/
-
-cat > rules.mk << 'EOF'
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
@@ -8,10 +5,9 @@ POINTING_DEVICE_DRIVER = pmw3360
 RGB_MATRIX_ENABLE = no
 LTO_ENABLE = yes
 MACRO_ENABLE = yes
-# Disable unused features to save flash & avoid conflicts
+EXTRAKEY_ENABLE = yes
 TAP_DANCE_ENABLE = no
 COMBO_ENABLE = no
 QMK_SETTINGS = no
 SPACE_CADET_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
-EOF
