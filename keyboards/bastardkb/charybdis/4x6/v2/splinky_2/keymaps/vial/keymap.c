efa14b8edf0fd82c6f7baa492e09ad75e361bbdb
KSP-Hub <karamin.stanislav@gmail.com>
2026-07-20 20:23:41 +0300

    feat: replace numpad keys with standard numbers (no NumLock needed), enable Vial GUI for non-technical users

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/keymap.c
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [6] = LAYOUT(
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT,                       QK_BOOT, KC_NO,   EE_CLR,  KC_NO,   KC_NO,   KC_TRNS,
-        KC_NO,   KC_P9,   KC_P0,   KC_NO,   KC_NO,   KC_NO,                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_PEQL,                       KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO,
-        KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_PMNS,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_9,   KC_0,   KC_NO,   KC_NO,   KC_NO,                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_5,   KC_6,   KC_7,   KC_8,   KC_EQL,                       KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO,
+        KC_NO,   KC_1,   KC_2,   KC_3,   KC_4,   KC_MINS,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,  KC_NO,  KC_NO,   KC_NO,                                           KC_NO,  KC_NO,   KC_NO,   KC_NO
     )
 };
